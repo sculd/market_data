294947867459dddef72522aef940ed752a238a0d
Hyungjun Lim <sculd3@gmail.com>
2024-04-01 21:13:21 -0400

    validate

---
+++ ingest/bq/validate.py
@@ -0,0 +1,29 @@
+import datetime, logging, os
+
+
+from . import cache
+from . import common
+from .common import AGGREGATION_MODE
+from ..util import time as util_time
+
+
+
+def verify_data_cache(
+    date_str_from: str,
+    date_str_to: str,
+    dataset_mode: common.DATASET_MODE,
+    export_mode: common.EXPORT_MODE,
+    aggregation_mode: AGGREGATION_MODE,
+):
+    t_id = common.get_full_table_id(dataset_mode, export_mode)
+    t_from, t_to = util_time.to_t(
+        date_str_from=date_str_from,
+        date_str_to=date_str_to,
+    )
+
+    t_ranges = cache.split_t_range(t_from, t_to)
+    for t_range in t_ranges:
+        t_from, t_to = t_range[0], t_range[1]
+        filename = cache.to_filename(t_id, aggregation_mode, t_from, t_to)
+        if not os.path.exists(filename):
+            logging.info(f"{filename=} does not exist.")
