88edcaab5989426bea2c24bfb812507d40cb7526
Hyungjun Lim <sculd3@gmail.com>
2024-12-07 18:08:45 -0500

    add ingest/gcs module

---
+++ ingest/gcs/util.py
@@ -0,0 +1,44 @@
+import datetime
+import os
+
+from google.cloud import storage
+
+_storage_client = storage.Client()
+_gcs_bucket_name = "algo_cache"
+_gcs_bucket = _storage_client.bucket(_gcs_bucket_name)
+
+
+def get_gcsblobname(label: str, t_id: str, t_from: datetime.datetime, t_to: datetime.datetime) -> str:
+    t_str_from = t_from.strftime("%Y-%m-%dT%H:%M:%S%z")
+    t_str_to = t_to.strftime("%Y-%m-%dT%H:%M:%S%z")
+    return os.path.join(label, f"{t_id}", f"{t_str_from}_{t_str_to}.parquet")
+
+def if_blob_exist(blob_name: str) -> bool:
+    return storage.Blob(bucket=_gcs_bucket, name=blob_name).exists(_storage_client)
+
+
+def download_gcs_blob(source_blob_name, destination_file_name):
+    bucket = _storage_client.bucket(_gcs_bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+    print(
+        f"Downloaded storage object {source_blob_name} from bucket {_gcs_bucket_name} to local file {destination_file_name}."
+    )
+
+def upload_file_to_public_gcs_bucket(local_filename, gcs_filename, rewrite=False) -> None:
+    # uoload the file to gcs
+    if storage.Blob(bucket=_gcs_bucket, name=gcs_filename).exists(_storage_client):
+        if rewrite:
+            blob = _gcs_bucket.blob(gcs_filename)
+            blob.delete(if_generation_match=None)
+        else:
+            print(f'{gcs_filename} already present in the bucket {_gcs_bucket_name} thus not proceeding further for {property}')
+            return
+
+    blob = _gcs_bucket.blob(gcs_filename)
+    generation_match_precondition = 0
+    blob.upload_from_filename(local_filename, if_generation_match=generation_match_precondition)
+
+    print(
+        f"File {local_filename} uploaded to {_gcs_bucket_name}/{gcs_filename}."
+    )
