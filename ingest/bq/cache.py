a14681df78aaec9843daecd0ce226e0eff0aeb00
Hyungjun Lim <sculd3@gmail.com>
2024-12-07 18:04:24 -0500

    move the cach under algo_cache, makae functions more generic

--- ingest/bq/cache.py
+++ ingest/bq/cache.py
@@ -10,18 +10,23 @@ from . import orderbook1l
 from . import common
 from .common import AGGREGATION_MODE
 from ..util import time as util_time
+from ..gcs.util import get_gcsblobname, download_gcs_blob, upload_file_to_public_gcs_bucket, if_blob_exist
+from google.cloud import storage
+
 
 # the cache will be stored per day.
 _cache_interval = datetime.timedelta(days=1)
-_cache_base_path = os.path.expanduser('~/market_data_cache')
+_cache_base_path = os.path.expanduser('~/algo_cache')
 try:
     os.mkdir(_cache_base_path)
 except FileExistsError:
     pass
-_label_no_label = ""
+_label_market_data = "market_data"
 
 # the timezone of dividing range into days.
 _cache_timezone = pytz.timezone('America/New_York')
+
+_timestamp_index_name = 'timestamp'
 
 
 def to_filename(basedir: str, label: str, t_id: str, aggregation_mode: AGGREGATION_MODE, t_from: datetime.datetime, t_to: datetime.datetime) -> str:
@@ -78,14 +83,16 @@ def is_exact_cache_interval(t_from: datetime.datetime, t_to: datetime.datetime) 
     return True
 
 
-def _cache_df(df: pd.DataFrame, aggregation_mode: AGGREGATION_MODE, t_id: str, t_from: datetime.datetime, t_to: datetime.datetime, overwrite=True):
+def _cache_df_daily(df: pd.DataFrame, label: str, aggregation_mode: AGGREGATION_MODE, t_id: str, t_from: datetime.datetime, t_to: datetime.datetime, overwrite=True):
     if not is_exact_cache_interval(t_from, t_to):
         logging.info(f"{t_from}-{t_to} does not match {_cache_interval=} thus will not be cached.")
         return None
+
     if len(df) == 0:
         logging.info(f"df for {t_from}-{t_to} is empty thus will not be cached.")
         return None
-    filename = to_filename(_cache_base_path, _label_no_label, t_id, aggregation_mode, t_from, t_to)
+
+    filename = to_filename(_cache_base_path, _label_market_data, t_id, aggregation_mode, t_from, t_to)
     if os.path.exists(filename):
         logging.info(f"{filename} already exists.")
         if overwrite:
@@ -96,8 +103,11 @@ def _cache_df(df: pd.DataFrame, aggregation_mode: AGGREGATION_MODE, t_id: str, t
     else:
         df.to_parquet(filename)
 
+    blob_name = get_gcsblobname(label, t_id, t_from, t_to)
+    upload_file_to_public_gcs_bucket(filename, blob_name, rewrite=overwrite)
 
-def _fetch_from_cache(
+
+def _fetch_from_daily_cache(
         t_id: str,
         label: str,
         aggregation_mode: AGGREGATION_MODE,
@@ -110,7 +120,13 @@ def _fetch_from_cache(
         return None
     filename = to_filename(_cache_base_path, label, t_id, aggregation_mode, t_from, t_to)
     if not os.path.exists(filename):
-        return None
+        blob_name = get_gcsblobname(label, t_id, t_from, t_to)
+        blob_exist = if_blob_exist(blob_name)
+        logging.info(f"{filename=} does not exist in local cache. For gcs, {blob_exist=}.")
+        if blob_exist:
+            download_gcs_blob(blob_name, filename)
+        else:
+            return None
     df = pd.read_parquet(filename)
     if len(df) == 0:
         return None
@@ -126,7 +142,8 @@ def read_from_cache(
         dataset_mode: common.DATASET_MODE,
         export_mode: common.EXPORT_MODE,
         aggregation_mode: AGGREGATION_MODE,
-        label: str = _label_no_label,
+        label: str = _label_market_data,
+        resample_interval_str = None,
         t_from: datetime.datetime = None,
         t_to: datetime.datetime = None,
         epoch_seconds_from: int = None,
@@ -147,7 +164,7 @@ def read_from_cache(
     t_ranges = split_t_range(t_from, t_to)
     df_concat = None
     for t_range in t_ranges:
-        df = _fetch_from_cache(t_id, label, aggregation_mode, t_range[0], t_range[1])
+        df = _fetch_from_daily_cache(t_id, label, aggregation_mode, t_range[0], t_range[1])
         if df is None:
             continue
         if df_concat is None:
@@ -156,17 +173,18 @@ def read_from_cache(
             df_concat = pd.concat([df_concat, df])
         del df
 
-    if df_concat is None:
-        return None
-    df_concat = df_concat.reset_index().groupby('symbol').apply(lambda x: x.set_index('timestamp').resample('1min').asfreq().ffill()).drop(columns='symbol').reset_index()
+    if df_concat is not None and resample_interval_str is not None:
+        df_concat = df_concat.reset_index().groupby('symbol').apply(
+            lambda x: x.set_index(_timestamp_index_name).resample(resample_interval_str).asfreq().ffill()).drop(columns='symbol').reset_index()
     return df_concat
 
 
-def fetch_and_cache(
+def query_and_cache(
         dataset_mode: common.DATASET_MODE,
         export_mode: common.EXPORT_MODE,
         aggregation_mode: AGGREGATION_MODE,
-        label: str = _label_no_label,
+        label: str = _label_market_data,
+        resample_interval_str = None,
         t_from: datetime.datetime = None,
         t_to: datetime.datetime = None,
         epoch_seconds_from: int = None,
@@ -199,10 +217,10 @@ def fetch_and_cache(
     for i, t_range in enumerate(t_ranges):
         if skip_first_day and i == 0:
             continue
-        df_cache = _fetch_from_cache(t_id, label, aggregation_mode, t_range[0], t_range[1])
+        df_cache = _fetch_from_daily_cache(t_id, label, aggregation_mode, t_range[0], t_range[1])
         if overwirte_cache or df_cache is None:
             df = fetch_function(t_id, aggregation_mode, t_from=t_range[0], t_to=t_range[1])
-            _cache_df(df, aggregation_mode, t_id, t_range[0], t_range[1])
+            _cache_df_daily(df, label, aggregation_mode, t_id, t_range[0], t_range[1])
         else:
             df = df_cache
 
@@ -210,18 +228,17 @@ def fetch_and_cache(
             continue
         if len(df) == 0:
             continue
-        if 'timestamp' in df.columns:
-            df = df.set_index('timestamp')
+        if _timestamp_index_name in df.columns:
+            df = df.set_index(_timestamp_index_name)
         if df_concat is None:
             df_concat = df.copy()
         else:
             df_concat = pd.concat([df_concat, df])
-
         del df
 
-    if df_concat is None:
-        return None
-    df_concat = df_concat.reset_index().groupby('symbol').apply(lambda x: x.set_index('timestamp').resample('1min').asfreq().ffill()).drop(columns='symbol').reset_index()
+    if df_concat is not None and resample_interval_str is not None:
+        df_concat = df_concat.reset_index().groupby('symbol').apply(
+            lambda x: x.set_index(_timestamp_index_name).resample(resample_interval_str).asfreq().ffill()).drop(columns='symbol').reset_index()
     return df_concat
 
 
