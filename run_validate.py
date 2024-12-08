460d3693f468e59347069c34cf2fa5a37470963f
Hyungjun Lim <sculd3@gmail.com>
2024-12-08 16:40:39 -0500

    validate withint cache module

--- run_validate.py
+++ run_validate.py
@@ -16,14 +16,14 @@ logging.basicConfig(
     ]
 )
 
-import ingest.bq.validate
+import ingest.bq.cache
 import ingest.bq.common
 
 if __name__ == '__main__':
-    ingest.bq.validate.verify_data_cache(
+    ingest.bq.cache.validate_df(
+        label=ingest.bq.cache._label_market_data,
         date_str_from='2024-01-01',
         date_str_to='2024-03-21',
         dataset_mode=ingest.bq.common.DATASET_MODE.OKX,
         export_mode=ingest.bq.common.EXPORT_MODE.BY_MINUTE,
-        aggregation_mode=ingest.bq.common.AGGREGATION_MODE.TAKE_LASTEST,
     )
