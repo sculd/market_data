2c2e2255cdf98df9de19a313dee7fc945ff3ca9b
Hyungjun Lim <sculd3@gmail.com>
2025-04-04 16:53:45 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="market_data",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "numba",
+        "google-cloud-storage",
+        "google-cloud-bigquery",
+        "google-cloud-bigquery[pandas]",
+        "pytz",
+    ],
+    extras_require={
+        'docs': [
+            'sphinx>=7.2.6',
+            'sphinx-rtd-theme>=2.0.0',
+            'sphinx-autodoc-typehints>=1.25.2',
+            'sphinx-copybutton>=0.5.2',
+            'sphinx-toolbox>=3.5.0',
+        ],
+    },
+    author="HyungJun Lim",
+    author_email="sculd3@gmail.com",
+    description="A package for market, feature, and machine learning data generation",
+    keywords="market data, finance, feature engineering, machine learning",
+    python_requires=">=3.7",
+) 
