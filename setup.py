d1a0bb360b96a3ed8d9fc3c9b7e02e799a421954
Hyungjun Lim <sculd3@gmail.com>
2025-04-12 08:49:12 -0400

    v.0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="market_data",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     install_requires=[
         "pandas",
