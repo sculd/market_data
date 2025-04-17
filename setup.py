fadf300e3588ba515b2aa55bb4c3f4849f1b5911
Hyungjun Lim <sculd3@gmail.com>
2025-04-16 22:10:13 -0400

    update setup with new version for seq ml data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="market_data",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     install_requires=[
         "pandas",
