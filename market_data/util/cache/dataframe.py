e213bc8ee9d6b4d7c0c64ca952e5e9209f64f64b
Hyungjun Lim <sculd3@gmail.com>
2025-07-19 15:50:45 -0400

    minor refactoring

--- market_data/util/cache/dataframe.py
+++ market_data/util/cache/dataframe.py
@@ -56,6 +56,50 @@ def cache_daily_df(df: pd.DataFrame, label: str, t_from: datetime.datetime, t_to
         df.to_parquet(filename)
         return None
 
+
+def cache_data_by_day(df: pd.DataFrame, label: str, t_from: datetime.datetime, t_to: datetime.datetime, 
+                     params_dir: str = None, overwrite=False, warm_up_period_days=1, 
+                     dataset_id: str = None, dataset_mode: DATASET_MODE = None, 
+                     export_mode: EXPORT_MODE = None, aggregation_mode: AGGREGATION_MODE = None,
+                     cache_base_path: str = None) -> None:
+    """Cache a DataFrame, splitting it into daily pieces"""
+    if len(df) == 0:
+        logger.info(f"df is empty for {label} thus will be skipped.")
+        return
+
+    def _split_df_by_day() -> List[pd.DataFrame]:
+        # Get timestamp column from index or regular column
+        if isinstance(df.index, pd.DatetimeIndex) or (df.index.nlevels > 1 and TIMESTAMP_INDEX_NAME in df.index.names):
+            timestamps = df.index.get_level_values(TIMESTAMP_INDEX_NAME) if df.index.nlevels > 1 else df.index
+            return [group[1] for group in df.groupby(timestamps.date)]
+        elif TIMESTAMP_INDEX_NAME in df.columns:
+            return [group[1] for group in df.groupby(df[TIMESTAMP_INDEX_NAME].dt.date)]
+        else:
+            logger.error(f"DataFrame doesn't have timestamp index or column for {label}")
+            return [df]  # Return whole df as one group
+
+    df_dailys = _split_df_by_day()
+    for i, df_daily in enumerate(df_dailys):
+        if i < warm_up_period_days:
+            continue
+
+        # Find time range for this daily df
+        if isinstance(df_daily.index, pd.DatetimeIndex) or (df_daily.index.nlevels > 1 and TIMESTAMP_INDEX_NAME in df_daily.index.names):
+            timestamps = df_daily.index.get_level_values(TIMESTAMP_INDEX_NAME) if df_daily.index.nlevels > 1 else df_daily.index
+        elif TIMESTAMP_INDEX_NAME in df_daily.columns:
+            timestamps = df_daily[TIMESTAMP_INDEX_NAME]
+        else:
+            logger.error(f"Cannot determine timestamps for daily data in {label}")
+            continue
+            
+        t_begin = anchor_to_begin_of_day(timestamps.min())
+        t_end = anchor_to_begin_of_day(t_begin + CACHE_INTERVAL)
+        cache_daily_df(df_daily, label, t_begin, t_end, params_dir, overwrite=overwrite, dataset_id=dataset_id,
+                       dataset_mode=dataset_mode, export_mode=export_mode, aggregation_mode=aggregation_mode,
+                       cache_base_path=cache_base_path)
+        del df_daily 
+
+
 def fetch_from_daily_cache(label: str, t_from: datetime.datetime, t_to: datetime.datetime, 
                           params_dir: str = None, columns: List[str] = None, 
                           dataset_id: str = None, dataset_mode: DATASET_MODE = None, 
@@ -132,46 +176,3 @@ def read_multithreaded(read_func: Callable, time_range: TimeRange, max_workers: 
         result_df = pd.DataFrame()
 
     return result_df
-        
-
-def cache_data_by_day(df: pd.DataFrame, label: str, t_from: datetime.datetime, t_to: datetime.datetime, 
-                     params_dir: str = None, overwrite=False, warm_up_period_days=1, 
-                     dataset_id: str = None, dataset_mode: DATASET_MODE = None, 
-                     export_mode: EXPORT_MODE = None, aggregation_mode: AGGREGATION_MODE = None,
-                     cache_base_path: str = None) -> None:
-    """Cache a DataFrame, splitting it into daily pieces"""
-    if len(df) == 0:
-        logger.info(f"df is empty for {label} thus will be skipped.")
-        return
-
-    def _split_df_by_day() -> List[pd.DataFrame]:
-        # Get timestamp column from index or regular column
-        if isinstance(df.index, pd.DatetimeIndex) or (df.index.nlevels > 1 and TIMESTAMP_INDEX_NAME in df.index.names):
-            timestamps = df.index.get_level_values(TIMESTAMP_INDEX_NAME) if df.index.nlevels > 1 else df.index
-            return [group[1] for group in df.groupby(timestamps.date)]
-        elif TIMESTAMP_INDEX_NAME in df.columns:
-            return [group[1] for group in df.groupby(df[TIMESTAMP_INDEX_NAME].dt.date)]
-        else:
-            logger.error(f"DataFrame doesn't have timestamp index or column for {label}")
-            return [df]  # Return whole df as one group
-
-    df_dailys = _split_df_by_day()
-    for i, df_daily in enumerate(df_dailys):
-        if i < warm_up_period_days:
-            continue
-
-        # Find time range for this daily df
-        if isinstance(df_daily.index, pd.DatetimeIndex) or (df_daily.index.nlevels > 1 and TIMESTAMP_INDEX_NAME in df_daily.index.names):
-            timestamps = df_daily.index.get_level_values(TIMESTAMP_INDEX_NAME) if df_daily.index.nlevels > 1 else df_daily.index
-        elif TIMESTAMP_INDEX_NAME in df_daily.columns:
-            timestamps = df_daily[TIMESTAMP_INDEX_NAME]
-        else:
-            logger.error(f"Cannot determine timestamps for daily data in {label}")
-            continue
-            
-        t_begin = anchor_to_begin_of_day(timestamps.min())
-        t_end = anchor_to_begin_of_day(t_begin + CACHE_INTERVAL)
-        cache_daily_df(df_daily, label, t_begin, t_end, params_dir, overwrite=overwrite, dataset_id=dataset_id,
-                       dataset_mode=dataset_mode, export_mode=export_mode, aggregation_mode=aggregation_mode,
-                       cache_base_path=cache_base_path)
-        del df_daily 
