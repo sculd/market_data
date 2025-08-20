82c9b2830ebbc787096364d8f1aa2f3f554a35a8
Hyungjun Lim <sculd3@gmail.com>
2025-08-19 23:28:50 -0400

    update version 0.6.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="market_data",
-    version="0.6.0",
+    version="0.6.2",
     packages=find_packages(),
     install_requires=[
         "pandas",
